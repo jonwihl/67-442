c19d5b23488369b707a3c0a01fc656cc27fb0332
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-10-30 19:28:11 -0400

    Initial Commit

---
+++ rentBetter/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rentBetter
+//
+//  Created by Arnav Jain on 10/30/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		// Do any additional setup after loading the view, typically from a nib.
+	}
+
+
+}
+
