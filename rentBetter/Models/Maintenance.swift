2c6c31c1f1422693dada53f9c45aa1e1fb7d30ac
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-10-31 19:30:17 -0400

    Sorry dad

---
+++ rentBetter/Models/Maintenance.swift
@@ -0,0 +1,16 @@
+//
+//  Maintenance.swift
+//  rentBetter
+//
+//  Created by Arnav Jain on 10/30/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Photos
+import UIKit
+
+struct Maintenance {
+	let title: String
+	let description: String
+	let photo: UIImage?
+}
