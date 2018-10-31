c4cfbcb5a472470afdd3bdc346250f2df04c4736
jonwihl <jonathanwihl@gmail.com>
2018-10-31 19:42:53 -0400

    Merged AJ's commits; Added Property Model

---
+++ rentBetter/Models/Property.swift
@@ -0,0 +1,23 @@
+//
+//  Property.swift
+//  rentBetter
+//
+//  Created by Jonathan Wihl on 10/31/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
+
+struct Property{
+    var addr_1: String
+    var addr_2: String?
+    var street: String
+    var zipcode: String
+    var city: String
+    var state: String
+    var rent: String?
+    var start_date: Date
+    var end_date: Date
+    
+    
+}
