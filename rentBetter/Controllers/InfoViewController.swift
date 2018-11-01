cf24d15da1adb3ebb4f4bfea578d18c6e7567d77
jonwihl <jonathanwihl@gmail.com>
2018-10-31 20:34:34 -0400

    Completed property labels

--- rentBetter/Controllers/InfoViewController.swift
+++ rentBetter/Controllers/InfoViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class InfoViewController: UIViewController {
     
-    var property = Property(addr_1: "Wilkins Ave", rent: "5000")
+    var property = Property(addr_1: "5445", street: "Wilkins Ave", zipcode: "15217", city: "Pittsburgh", state: "PA", rent: "5000", start_date: "10/31/2018", end_date: "10/31/2019")
 	
     @IBOutlet weak var rent_Label: UILabel!
     @IBOutlet weak var Addr_1_Label: UILabel!
     
+    @IBOutlet weak var city_Label: UILabel!
+    @IBOutlet weak var zipcode_Label: UILabel!
+    
+    @IBOutlet weak var state_Label: UILabel!
+    
+    @IBOutlet weak var startDate_Label: UILabel!
+    @IBOutlet weak var endDate_Label: UILabel!
     
     override func viewDidLoad() {
 		super.viewDidLoad()
@@ -23,7 +30,14 @@ class InfoViewController: UIViewController {
 	}
     
     func updateLabelAddr() -> Void {
-        Addr_1_Label.text = property.rent
+        Addr_1_Label.text = property.addr_1 + property.street
+        zipcode_Label.text = property.zipcode
+        state_Label.text = property.state
+        startDate_Label.text = property.start_date
+        endDate_Label.text = property.end_date
+        rent_Label.text = property.rent
+        city_Label.text = property.city
+    
     }
     
     
--- rentBetter/Models/Property.swift
+++ rentBetter/Models/Property.swift
@@ -10,17 +10,23 @@ import Foundation
 
 public class Property{
     var addr_1: String
-//    var addr_2: String?
-//    var street: String
-//    var zipcode: String
-//    var city: String
-//    var state: String
+    var addr_2: String?
+    var street: String
+    var zipcode: String
+    var city: String
+    var state: String
     var rent: String?
-//    var start_date: Date
-//    var end_date: Date
+    var start_date: String
+    var end_date: String
     
-    init(addr_1: String, rent: String) {
+    init(addr_1: String, street:String, zipcode:String, city:String, state:String, rent: String, start_date: String, end_date: String) {
         self.addr_1 = addr_1
         self.rent = rent
+        self.street = street
+        self.zipcode = zipcode
+        self.city = city
+        self.state = state
+        self.start_date = start_date
+        self.end_date = end_date
     }
 }
