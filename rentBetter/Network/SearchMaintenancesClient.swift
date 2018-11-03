a88850ad48cec5089dd168634a548f80b1bbe867
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-11-03 16:26:47 -0400

    added files for Network and Parser

---
+++ rentBetter/Network/SearchMaintenancesClient.swift
@@ -0,0 +1,9 @@
+//
+//  SearchMaintenancesClient.swift
+//  rentBetter
+//
+//  Created by Arnav Jain on 11/3/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
---
+++ rentBetter/Network/SearchPropertiesClient.swift
@@ -0,0 +1,10 @@
+//
+//  SearchPropertiesClient.swift
+//  rentBetter
+//
+//  Created by Arnav Jain on 11/3/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
+
