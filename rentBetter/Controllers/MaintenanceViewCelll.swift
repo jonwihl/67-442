390b4ed51982cc41199ee2ffe14102d0bc3e9fe8
jonwihl <jonathanwihl@gmail.com>
2018-11-03 15:21:29 -0400

    Tenant and Landlord story boards

--- rentBetter/Controllers/MaintenanceViewCelll.swift
+++ rentBetter/Controllers/MaintenanceViewCelll.swift
@@ -10,7 +10,6 @@ import UIKit
 
 class MaintenanceViewCelll: UITableViewCell {
     
-    let maintenance = Maintenance(title: "Broken Sink", created_on: "11/1/2018")
 
     @IBOutlet weak var createdOnLabel: UILabel!
     @IBOutlet weak var titleLabel: UILabel!
@@ -22,14 +21,9 @@ class MaintenanceViewCelll: UITableViewCell {
 
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-        updateCellLabel()
 
         // Configure the view for the selected state
     }
     
-    func updateCellLabel() -> Void {
-        createdOnLabel.text = maintenance.created_on
-        titleLabel.text = maintenance.title
-    }
-
+    
 }
--- rentBetter/Controllers/MaintenanceViewController.swift
+++ rentBetter/Controllers/MaintenanceViewController.swift
@@ -14,6 +14,10 @@ class MaintenanceViewController: UIViewController {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 	}
+    
+    
+
+
 	
 	
 }
