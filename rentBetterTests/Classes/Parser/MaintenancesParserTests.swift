83736905014e0910f739af82918d572da2772044
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-11-03 19:22:41 -0400

    new test files added

---
+++ rentBetterTests/Classes/Parser/MaintenancesParserTests.swift
@@ -0,0 +1,9 @@
+//
+//  MaintenancesParserTests.swift
+//  rentBetterTests
+//
+//  Created by Arnav Jain on 11/3/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
---
+++ rentBetterTests/Classes/Parser/PropertiesParserTests.swift
@@ -0,0 +1,9 @@
+//
+//  PropertiesParserTests.swift
+//  rentBetterTests
+//
+//  Created by Arnav Jain on 11/3/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
