49d14761c28f60c7cb7977093b0c2356198b318a
jonwihl <jonathanwihl@gmail.com>
2018-11-04 16:18:08 -0500

    Landlord maintenances view model tests

--- rentBetterTests/Classes/Maintenances View Models/LandlordMaintenancesViewModelTests.swift
+++ rentBetterTests/Classes/Maintenances View Models/LandlordMaintenancesViewModelTests.swift
@@ -1,9 +1,37 @@
 //
-//  LandlordMaintenancesViewModelTests.swift
+//  LandlordPropertiesViewModelTests.swift
 //  rentBetterTests
 //
 //  Created by Arnav Jain on 11/4/18.
 //  Copyright © 2018 AJ. All rights reserved.
 //
 
-import Foundation
+import XCTest
+@testable import rentBetter
+
+class LandlordMaintenancesViewModelTests: XCTestCase {
+    
+    func test_numberOfRows() {
+        let maintenances = [createMaintenance()]
+        let viewModel = LandlordMaintenancesViewModel()
+        viewModel.maintenances = maintenances
+        
+        XCTAssertEqual(viewModel.numberOfRows(), 1)
+    }
+    
+    func test_titleForRowAtIndexPath() {
+        let maintenance1 = createMaintenance()
+        let viewModel = LandlordMaintenancesViewModel()
+        viewModel.maintenances = [property1]
+        
+        let indexPath1 = IndexPath(row: 0, section: 0)
+        XCTAssertEqual(viewModel.addrForRowAtIndexPath(indexPath1), "Sink not running")
+    }
+    
+    
+    func createMaintenance() -> Maintenance {
+        return Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true)
+    }
+    
+}
+
