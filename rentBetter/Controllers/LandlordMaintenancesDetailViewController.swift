3c27b927cac9382f30fe57c4fff0664886f4090c
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-11-04 13:10:16 -0500

    fixing storyboards and controllers

---
+++ rentBetter/Controllers/LandlordMaintenancesDetailViewController.swift
@@ -0,0 +1,36 @@
+//
+//  LandlordMaintenancesDetailViewController.swift
+//  rentBetter
+//
+//  Created by Arnav Jain on 11/4/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+
+import UIKit
+
+class LandlordMaintenancesDetailViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		// Do any additional setup after loading the view.
+	}
+	
+	override func didReceiveMemoryWarning() {
+		super.didReceiveMemoryWarning()
+		// Dispose of any resources that can be recreated.
+	}
+	
+	
+	/*
+	// MARK: - Navigation
+	
+	// In a storyboard-based application, you will often want to do a little preparation before navigation
+	override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+	// Get the new view controller using segue.destinationViewController.
+	// Pass the selected object to the new view controller.
+	}
+	*/
+	
+}
--- rentBetter/Controllers/LandlordMaintenancesViewController.swift
+++ rentBetter/Controllers/LandlordMaintenancesViewController.swift
@@ -8,7 +8,7 @@
 
 import UIKit
 
-class LandlordMaintenanceViewController: UIViewController {
+class LandlordMaintenancesViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
