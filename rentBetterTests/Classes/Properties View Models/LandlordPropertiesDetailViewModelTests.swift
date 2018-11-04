df60e34be4ad734eb3537d06c5e12fe709ae02b2
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-11-04 13:56:46 -0500

    about to write tests for property view models

---
+++ rentBetterTests/Classes/Properties View Models/LandlordPropertiesDetailViewModelTests.swift
@@ -0,0 +1,9 @@
+//
+//  LandlordPropertiesDetailViewModelTests.swift
+//  rentBetterTests
+//
+//  Created by Arnav Jain on 11/4/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
