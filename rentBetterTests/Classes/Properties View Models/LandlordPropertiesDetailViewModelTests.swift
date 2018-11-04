6c46d10eeffcb931c520b567549074fdbe7767c4
jonwihl <jonathanwihl@gmail.com>
2018-11-04 15:10:24 -0500

    Landlord Properties Detail View Model tests

--- rentBetterTests/Classes/Properties View Models/LandlordPropertiesDetailViewModelTests.swift
+++ rentBetterTests/Classes/Properties View Models/LandlordPropertiesDetailViewModelTests.swift
@@ -9,3 +9,50 @@
 import XCTest
 @testable import rentBetter
 
+class LandlordPropertiesDetailViewModelTests: XCTestCase {
+    
+    let property = createProperty()
+    let viewModel = RepositoryDetailViewModel([property])
+    
+    
+    func createProperty() -> Property {
+        return Property(id: 1, addr_line1: "5445 Wilkins", addr_line2: "N/A", city: "Pittsburgh", state: "PA", zipcode: "15217", rent: 3900, start_date: "6/1/2018", end_date: "6/2/2019", active: true)
+    }
+    
+    func test_addrLine1() {
+        XCTAssertEqual("5445 Wilkins", viewModel.addr_line1())
+    }
+    
+    func test_addrLine2() {
+        XCTAssertEqual("N/A", viewModel.addr_line2)
+    }
+    
+    func test_city(){
+        XCTAssertEqual("Pittsburgh", viewModel.city)
+    }
+    
+    func test_state(){
+        XCTAssertEqual("PA", viewModel.state)
+    }
+    
+    func test_zipcode(){
+        XCTAssertEqual("15217", viewModel.zipcode)
+    }
+    
+    func test_rent(){
+        XCTAssertEqual(3900, viewModel.rent)
+    }
+    
+    func test_startDate(){
+        XCTAssertEqual("6/1/2018", viewModel.start_date)
+    }
+    
+    func test_endDate(){
+        XCTAssertEqual("6/2/2019", viewModel.end_date)
+    }
+
+    
+    
+}
+
+
