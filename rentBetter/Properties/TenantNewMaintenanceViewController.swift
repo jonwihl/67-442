b8bc0997b747c3ef985ea768d7a399a8501386d1
jonwihl <jonathanwihl@gmail.com>
2018-11-26 19:01:29 -0500

    Posting maintenenace request working with API

--- rentBetter/Properties/TenantNewMaintenanceViewController.swift
+++ rentBetter/Properties/TenantNewMaintenanceViewController.swift
@@ -11,26 +11,16 @@ import Alamofire
 
 class TenantNewMaintenanceViewController: UIViewController {
 
+    @IBOutlet weak var titleText: UITextField!
+    @IBOutlet weak var descriptionText: UITextField!
+    
     @IBAction func addJson(_ sender: Any) {
         print("posting params")
-//        let parameters = [
-//            "id":1,
-//            "name":"Jonathan",
-//            "phone":"6176105413",
-//            "username":"jonwihl1",
-//            "password" : "12345",
-//            "password_confirmation": "12345",
-//            "created_at":"2018-11-11T23:44:51.483Z",
-//            "updated_at":"2018-11-11T23:44:51.483Z"
-//            ] as [String : Any]
-        
         let  parameters = [
             "id":4,
-            "title":"Mold",
-            "description":"Mold in Basement.",
+            "title": titleText.text,
+            "description": descriptionText.text,
             "photo": "",
-            "created_on":"2018-01-11",
-            "closed_on": "",
             "active":true,
             "property_id":5,"tenant_id":10,
             "created_at":"2018-11-15T23:35:51.547Z",
