5b644e06c3567ccdb4b6fa5528abe249be38700f
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-11-27 19:08:11 -0500

    reminders parser and network completed

---
+++ rentBetter/Network/SearchRemindersClient.swift
@@ -0,0 +1,28 @@
+//
+//  SearchRemindersClient.swift
+//  rentBetter
+//
+//  Created by Arnav Jain on 11/27/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
+import Alamofire
+
+class SearchRemindersClient {
+	func fetchTenants(_ completion: @escaping (Data?) -> Void) {
+		
+		let urlString = "https://protected-gorge-28359.herokuapp.com/reminders"
+		
+		Alamofire.request(urlString).response { response in
+			if let error = response.error {
+				print("Error fetching tenants: \(error)")
+				completion(response.data)
+				return
+			}
+			completion(response.data)
+		}
+		
+	}
+}
+
---
+++ rentBetter/Parser/RemindersParser.swift
@@ -0,0 +1,63 @@
+//
+//  RemindersParser.swift
+//  rentBetter
+//
+//  Created by Arnav Jain on 11/27/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
+
+
+class RemindersParser {
+	
+	func parseDictionary(_ data: Data?) -> JSONArray? {
+		do {
+			if let data = data,
+				let json = try JSONSerialization.jsonObject(with: data, options: []) as? JSONArray {
+				return json
+			}
+		} catch {
+			print("Couldn't parse JSON. Error: \(error)")
+		}
+		return nil
+	}
+	
+	
+	func remindersFromSearchResponse(_ data: Data?) -> [Reminder]? {
+		guard let dict = parseDictionary(data) else {
+			print("Error: couldn't parse dictionary from data")
+			return nil
+		}
+		
+		//		guard let maintenanceDicts = dict["maintenances"] as? [JSONDictionary] else {
+		//			print("Error: couldn't parse maintenances from JSON: \(dict)")
+		//			return nil
+		//		}
+		
+		return dict.compactMap { parseReminder($0) }
+	}
+	
+	
+	
+	func parseReminder(_ dict: JSONDictionary) -> Maintenance? {
+		if let id = dict["id"] as? Int,
+			let title = dict["title"] as? String,
+			let description = dict["description"] as? String,
+			let created_on = dict["created_on"] as? String,
+			let removed_on = dict["closed_on"] as? String,
+			let active = dict["photo"] as? String,
+			let landlord_id = dict["landlord_id"] as? Int,
+			let property_id = dict["property_id"] as? Int{
+			//			display only the active maintenances
+				let reminder = Reminder(id: id, title: title, description: description,
+																			created_on: created_on, removed_on: removed_on,
+																			active: active, landlord_id: landlord_id, property_id:property_id)
+				return reminder
+		}
+		else {
+			print("Error: couldn't parse JSON dictionary: \(dict)")
+		}
+		return nil
+	}
+}
