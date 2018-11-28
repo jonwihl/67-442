6000969988087b4281cacccd9ea1b7aab162167d
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-11-27 23:48:50 -0500

    basic payments working

--- rentBetter/Payment/PaymentViewController.swift
+++ rentBetter/Payment/PaymentViewController.swift
@@ -12,6 +12,9 @@ import Stripe
 
 class PaymentViewController: UIViewController {
 	
+	@IBOutlet weak var paymentAmount: UITextField!
+	
+	
 	override func viewDidLoad() {
 		super.viewDidLoad()
 		
@@ -23,7 +26,44 @@ class PaymentViewController: UIViewController {
 		
 	}
 	
+	@IBAction func continuePayment(_ sender: Any) {
+		print("X")
+		let addCardViewController = STPAddCardViewController()
+		addCardViewController.delegate = self
+		navigationController?.pushViewController(addCardViewController, animated: true)
+	}
 	
 	
 	
+}
+
+extension PaymentViewController: STPAddCardViewControllerDelegate {
+	
+	func addCardViewControllerDidCancel(_ addCardViewController: STPAddCardViewController) {
+		navigationController?.popViewController(animated: true)
+	}
+	
+	func addCardViewController(_ addCardViewController: STPAddCardViewController,
+														 didCreateToken token: STPToken,
+														 completion: @escaping STPErrorBlock) {
+		StripeClient.shared.completeCharge(with: token, amount: Int((paymentAmount?.text!)!)!) { result in
+			switch result {
+			// 1
+			case .success:
+				completion(nil)
+				
+				let alertController = UIAlertController(title: "Congrats",
+																								message: "Your payment was successful!",
+																								preferredStyle: .alert)
+				let alertAction = UIAlertAction(title: "OK", style: .default, handler: { _ in
+					self.navigationController?.popViewController(animated: true)
+				})
+				alertController.addAction(alertAction)
+				self.present(alertController, animated: true)
+			// 2
+			case .failure(let error):
+				completion(error)
+			}
+		}
+	}
 }
