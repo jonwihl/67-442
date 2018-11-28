97b5acd558687a35f0e19a305cde3d843ce4f0f6
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-11-27 23:21:13 -0500

    progressing on Stripe

---
+++ rentBetter/Payment/PaymentViewController.swift
@@ -0,0 +1,29 @@
+//
+//  PaymentViewController.swift
+//  rentBetter
+//
+//  Created by Arnav Jain on 11/27/18.
+//  Copyright © 2018 AJ. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Stripe
+
+class PaymentViewController: UIViewController {
+	
+	override func viewDidLoad() {
+		super.viewDidLoad()
+		
+		title = "Payment"
+	}
+	
+	override func viewWillAppear(_ animated: Bool) {
+		super.viewWillAppear(animated)
+		
+	}
+	
+	
+	
+	
+}
