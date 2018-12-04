bddbff0571132036500cd52c99331dd2d89d4d0c
jonwihl <jonathanwihl@gmail.com>
2018-12-04 13:51:15 -0500

    Login ui improvements

--- rentBetter/LoginViewController.swift
+++ rentBetter/LoginViewController.swift
@@ -10,11 +10,18 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
-	override func viewDidLoad() {
+    @IBOutlet weak var passwordField: UITextField!
+    @IBOutlet weak var userNameField: UITextField!
+    
+    override func viewDidLoad() {
 		super.viewDidLoad()
 		// Do any additional setup after loading the view, typically from a nib.
 		
 		self.view.backgroundColor = UIColor(red: 76/100, green: 94/100, blue: 92/100, alpha: 1)
+        passwordField.text = "  Password"
+        passwordField.textColor = UIColor.lightGray
+        userNameField.text = "  Username"
+        userNameField.textColor = UIColor.lightGray
 	}
 
 
