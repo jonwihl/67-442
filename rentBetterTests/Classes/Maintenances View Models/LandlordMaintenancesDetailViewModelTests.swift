bd2c879503a351ec02effb242b212a255c3dd30c
Arnav Jain <arnavj1@andrew.cmu.edu>
2018-12-08 15:38:39 -0500

    fixing up some tests

--- rentBetterTests/Classes/Maintenances View Models/LandlordMaintenancesDetailViewModelTests.swift
+++ rentBetterTests/Classes/Maintenances View Models/LandlordMaintenancesDetailViewModelTests.swift
@@ -11,35 +11,55 @@ import XCTest
 
 class LandlordMaintenancesDetailViewModelTests: XCTestCase {
     
-    func test_title() {
-			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true)
+			func test_id() {
+				let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
+				let viewModel = LandlordMaintenancesDetailViewModel(maintenance: main)
+				XCTAssertEqual(1, viewModel.id())
+			}
+	
+		func test_title() {
+			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
         let viewModel = LandlordMaintenancesDetailViewModel(maintenance: main)
         XCTAssertEqual("Sink not running", viewModel.title())
     }
     
     func test_description(){
-			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true)
+			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
         let viewModel = LandlordMaintenancesDetailViewModel(maintenance: main)
         XCTAssertEqual("no water is coming out of the kitchen sink", viewModel.description())
     }
     
     func test_createdOn(){
-			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true)
+			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
         let viewModel = LandlordMaintenancesDetailViewModel(maintenance: main)
         XCTAssertEqual("10/15/2018", viewModel.created_on())
     }
     
     func test_closedOn(){
-			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true)
+			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
         let viewModel = LandlordMaintenancesDetailViewModel(maintenance: main)
         XCTAssertEqual("10/20/2018", viewModel.closed_on())
     }
     
     func test_photo(){
-			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true)
+			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
         let viewModel = LandlordMaintenancesDetailViewModel(maintenance: main)
         XCTAssertEqual("xyz", viewModel.photo())
     }
+	
+		func test_active(){
+			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
+			let viewModel = LandlordMaintenancesDetailViewModel(maintenance: main)
+			XCTAssertEqual(true, viewModel.active())
+		}
+	
+	
+		func test_deactive(){
+			let main = Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
+			let viewModel = LandlordMaintenancesDetailViewModel(maintenance: main)
+			viewModel.deactivate()
+			XCTAssertEqual(false, viewModel.active())
+		}
     
 }
 
--- rentBetterTests/Classes/Maintenances View Models/LandlordMaintenancesViewModelTests.swift
+++ rentBetterTests/Classes/Maintenances View Models/LandlordMaintenancesViewModelTests.swift
@@ -27,10 +27,20 @@ class LandlordMaintenancesViewModelTests: XCTestCase {
         let indexPath1 = IndexPath(row: 0, section: 0)
         XCTAssertEqual(viewModel.titleForRowAtIndexPath(indexPath1), "Sink not running")
     }
+	
+		func test_summaryForRowAtIndexPath() {
+			let maintenance1 = createMaintenance()
+			let viewModel = LandlordMaintenancesViewModel()
+			viewModel.maintenances = [maintenance1]
+			
+			let indexPath1 = IndexPath(row: 0, section: 0)
+			XCTAssertEqual(viewModel.summaryForRowAtIndexPath(indexPath1), "No water is coming out of the kitchen sink")
+		}
+		
     
     
     func createMaintenance() -> Maintenance {
-        return Maintenance(id: 1, title: "Sink not running", description: "no water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true)
+			return Maintenance(id: 1, title: "Sink not running", description: "No water is coming out of the kitchen sink", created_on: "10/15/2018", closed_on: "10/20/2018", photo: "xyz", active: true, property_id: 1, tenant_id: 1)
     }
     
 }
